d4ffbb29272d795f7501e2bb7cbdec7699d94fdd
Dilyan Svetiev <dsvetiev@gmail.com>
2024-11-06 17:23:25 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='note-management-container'>
+      <h3>Note management system</h3>
+    </div>
+  )
+}
+
+export default App
